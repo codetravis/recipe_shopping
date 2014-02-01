0d9b427f869e8050c052e46ef0d2ab0d1df39590
codesheep <traviscoop3r@gmail.com>
2014-02-01 12:55:46 -0500

    created simple login form

--- shopping.py
+++ shopping.py
@@ -5,14 +5,19 @@ db = web.database(dbn='mysql', user='test', pw='test123', db='recipe_shopping')
 render = web.template.render('templates/')
 
 urls = (
-    '/', 'index',
-    '/add', 'add'
+    '/(\d*)', 'index',
+    '/add', 'add',
+    '/login', 'login'
 )
 
 
 class index:
-    def GET(self):
-        users = db.select('users')
+    def GET(self, user_id):
+        if user_id:
+            where = "users.id = %s" % user_id
+        else:
+            where = None
+        users = db.select('users', where=where)
         return render.index(users)
 
 class add:
@@ -20,6 +25,21 @@ class add:
         i = web.input()
         n = db.insert('users', email=i.email, password=i.password)
         raise web.seeother('/')
+
+class login:
+    def GET(self):
+        return render.login()
+
+    def POST(self):
+        credentials = web.input()
+        q = web.SQLQuery(["SELECT * FROM users WHERE email=", 
+            web.SQLParam(credentials.email), " AND password=", 
+            web.SQLParam(credentials.password)])
+        user = db.query(q)
+        if len(user) > 0:
+            raise web.seeother('/' + str(user[0].id))
+        else:
+            return render.login()
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
